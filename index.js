19c536aabdc393a98ef94408b37337a1f0e1bd24
Hamza Saleem <hamzasaleemzpr@gmail.com>
2017-03-12 22:36:45 +0500

    asa

--- index.js
+++ index.js
@@ -17,7 +17,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use('/assets',express.static('assets'));
 
-app.use(bodyParser.urlencoded({extended: true}))
+
+// create application/json parser 
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+
 
 const httpServer = http.createServer(app);
 
@@ -36,6 +44,37 @@ console.log(`Web Server running on port ${process.env.PORT}` );
 });
 
 
+app.get('/login', function (request, response) {
+
+response.render('login');
+
+});
+
+
+
+app.post('/login', urlencodedParser ,function (request, response) {
+
+if (!request.body) return response.sendStatus(400)
+
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      
+      client.query('SELECT * FROM "USER" WHERE username = ${request.body.username} and password = ${request.body.password} ', function(err, result) {
+      done();
+
+      //console.log(result.rows[0]);
+
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { //response.render('index', {menu: result.rows} );
+           response.send(result.rows[0]);
+
+        }
+    });
+  });
+
+});
 
 
 
