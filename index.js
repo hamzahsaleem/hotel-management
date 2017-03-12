07a79e27bfd8c77881d6fd4a5b49d54831d81960
Hamza Saleem <hamzasaleemzpr@gmail.com>
2017-03-12 21:14:13 +0500

    aas

--- index.js
+++ index.js
@@ -36,14 +36,11 @@ console.log(`Web Server running on port ${process.env.PORT}` );
 });
 
 
+
+
+
+
 app.get('/', function (request, response) {
-
-    response.render('index');
-});
-
-
-
-app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
       
       client.query('SELECT * FROM "MENU"', function(err, result) {
