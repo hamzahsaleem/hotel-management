5a311ef3e0a9b0bb44a77c75aa15903d6744c833
Hamza Saleem <hamzasaleemzpr@gmail.com>
2017-03-12 19:17:35 +0500

    commit 5

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ app.get('/',function(req,res){
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+      console.log(process.env.DATABASE_URL);
+      client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
